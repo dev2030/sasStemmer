677d21eaf99d1b66aca4b8d27e2f94b76fe49941
dev2030 <62925328+dev2030@users.noreply.github.com>
2020-04-24 14:43:03 +0300

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,6 +1,6 @@
 # To filter Ararbic text and produce plain Arabic text
 clean(txt) {
-    t = txt
+    t = txt  # from artathon
     # finish and return
     # this is the last mile
     # until now, so far.
